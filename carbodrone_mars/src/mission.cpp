e6f37c8f6fbfa8216c558b6d746a1abcdd1de3a7
Carbon225 <carbon225@protonmail.com>
2024-06-14 03:16:11 +0200

    basic mission

--- carbodrone_mars/src/mission.cpp
+++ carbodrone_mars/src/mission.cpp
@@ -9,22 +9,40 @@
 
 #include "px4_msgs/msg/vehicle_command.hpp"
 #include "px4_msgs/msg/mode_completed.hpp"
+#include "px4_msgs/msg/vehicle_status.hpp"
 #include "px4_msgs/msg/vehicle_global_position.hpp"
+#include "px4_msgs/msg/vehicle_local_position.hpp"
 
 using namespace std::chrono_literals;
+using px4_msgs::msg::VehicleCommand;
+using px4_msgs::msg::ModeCompleted;
+using px4_msgs::msg::VehicleStatus;
+using px4_msgs::msg::VehicleGlobalPosition;
+using px4_msgs::msg::VehicleLocalPosition;
 
 #define MISSION_STATE_LIST \
     X(INIT)                \
     \
-    X(DO_TAKEOFF_ENTER)    \
-    X(DO_TAKEOFF_ARMED)    \
-    X(DO_TAKEOFF_LEAVE)    \
+    X(DO_TAKEOFF)          \
     \
-    X(DO_REPOSITION_ENTER) \
-    X(DO_REPOSITION_LEAVE) \
+    X(DO_MINE_SCAN_GOTO)   \
+    X(DO_MINE_SCAN_REBASE) \
     \
-    X(DO_PRECLAND_ENTER)   \
-    X(DO_PRECLAND_LEAVE)   \
+    X(DO_FIELD_SCAN_GOTO)  \
+    X(DO_FIELD_SCAN_REBASE)\
+    X(DO_FIELD_SCAN_DETECT)\
+    \
+    X(DO_BALL_PICKUP_LAND) \
+    X(DO_BALL_PICKUP_GRAB) \
+    \
+    X(DO_TAKEOFF_ARM)      \
+    \
+    X(DO_BARREL_GOTO)      \
+    X(DO_BARREL_AIM)       \
+    X(DO_BARREL_DROP)      \
+    X(DO_BARREL_CLEAR)     \
+    \
+    X(DO_RTL)              \
     \
     X(DONE)                \
     X(NOOP)
@@ -42,7 +60,11 @@ static const char *MissionStateName[] = {
 #undef X
 };
 
-static const double BARREL_WAYPOINT[2] = {47.398132805733496, 8.54616487844852};
+#include "state_machine_node.hpp"
+
+static const double BARREL_WAYPOINT[3] = {47.398132805733496, 8.54616487844852, 10.0};
+
+static const double MINE_WAYPOINT[3] = {47.39796990342122, 8.546164727922147, 15.0};
 
 static const double FIELD_WAYPOINTS[9][2] = {
   {47.39794428391416,
@@ -75,69 +97,15 @@ static const double FIELD_WAYPOINTS[9][2] = {
   8.546203601239528}
 };
 
-class StateMachineNode : public rclcpp::Node
-{
-protected:
-    StateMachineNode(const std::string &name)
-        : Node(name)
-    {
-        _loop_timer = create_wall_timer(100ms, std::bind(&StateMachineNode::loop, this));
-    }
+static const double MISSION_START_ALT = 10.0;
+static const double FIELD_SCAN_ALT = 5.0;
+static const double BARREL_AIM_ALT = 1.0;
+static const double BARREL_HEIGHT = 1.0;
+static const int GRABBER_DELAY = 20;
 
-    void change_state(MissionState new_state)
-    {
-        RCLCPP_INFO(get_logger(), "State change: %s -> %s", MissionStateName[(int)_state], MissionStateName[(int)new_state]);
-        _state = new_state;
-        _state_delay = false;
-        _state_condition = false;
-    }
-
-    void change_state_after(MissionState new_state, int delay)
-    {
-        RCLCPP_INFO(get_logger(), "Delay %d ticks", delay);
-        _next_state = new_state;
-        _delay_counter = delay;
-        _state_delay = true;
-        _state_condition = false;
-    }
-
-    void change_state_after_condition(MissionState new_state, std::function<bool()> condition)
-    {
-        RCLCPP_INFO(get_logger(), "Wait for condition");
-        _next_state = new_state;
-        _delay_condition = condition;
-        _state_delay = false;
-        _state_condition = true;
-    }
-
-    virtual void process_state(MissionState state) = 0;
-
-private:
-    void loop()
-    {
-        if (_state_delay)
-        {
-            if (_delay_counter-- > 0)
-                return process_state(MissionState::NOOP);
-            else change_state(_next_state);
-        }
-        else if (_state_condition)
-        {
-            if (!_delay_condition())
-                return process_state(MissionState::NOOP);
-            else change_state(_next_state);
-        }
-        process_state(_state);
-    }
-
-    MissionState _state = MissionState::INIT;
-    bool _state_delay = false;
-    bool _state_condition = false;
-    int _delay_counter;
-    std::function<bool()> _delay_condition;
-    MissionState _next_state;
-    rclcpp::TimerBase::SharedPtr _loop_timer;
-};
+static const double GLOBAL_LAT_ACCEPTANCE = 0.0001;
+static const double GLOBAL_LON_ACCEPTANCE = 0.0001;
+static const float GLOBAL_ALT_ACCEPTANCE = 0.1;
 
 class MissionNode : public StateMachineNode
 {
@@ -149,20 +117,28 @@ public:
         _tf_listener = std::make_unique<tf2_ros::TransformListener>(*_tf_buf);
         _tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(this);
 
-        _command_pub = create_publisher<px4_msgs::msg::VehicleCommand>(
+        _command_pub = create_publisher<VehicleCommand>(
             "/fmu/in/vehicle_command", rclcpp::ServicesQoS());
 
-        _mode_completed_sub = create_subscription<px4_msgs::msg::ModeCompleted>(
+        _mode_completed_sub = create_subscription<ModeCompleted>(
             "/fmu/out/mode_completed",
             rclcpp::SensorDataQoS(),
             std::bind(&MissionNode::on_mode_completed, this, std::placeholders::_1));
 
-        _global_position_sub = create_subscription<px4_msgs::msg::VehicleGlobalPosition>(
+        _vehicle_status_sub = create_subscription<VehicleStatus>(
+            "/fmu/out/vehicle_status",
+            rclcpp::SensorDataQoS(),
+            std::bind(&MissionNode::on_vehicle_status, this, std::placeholders::_1));
+
+        _global_position_sub = create_subscription<VehicleGlobalPosition>(
             "/fmu/out/vehicle_global_position",
             rclcpp::SensorDataQoS(),
             std::bind(&MissionNode::on_global_position, this, std::placeholders::_1));
 
-        _mission_altitude = declare_parameter("mission_altitude", 5.0);
+        _local_position_sub = create_subscription<VehicleLocalPosition>(
+            "/fmu/out/vehicle_local_position",
+            rclcpp::SensorDataQoS(),
+            std::bind(&MissionNode::on_local_position, this, std::placeholders::_1));
     }
 
 private:
@@ -171,32 +147,30 @@ private:
         switch (state)
         {
         case MissionState::INIT:
-            change_state_after(MissionState::ARM, 20);
-            break;
-
-        case MissionState::ARM:
-            RCLCPP_INFO(get_logger(), "Arming");
-            do_arm();
-            change_state_after(MissionState::TAKEOFF, 30);
-            break;
-
-        case MissionState::TAKEOFF:
-            RCLCPP_INFO(get_logger(), "Takeoff to %.2f meters", _mission_altitude);
-            do_takeoff(_mission_altitude);
-
-            _mode_completed = false;
-            change_state_after_condition(MissionState::GOTO_FIELD, [this](){
-                return _mode_completed;
+            change_state_after_condition(MissionState::DO_TAKEOFF, [this](){
+                return _is_armed;
             });
             break;
 
-        case MissionState::GOTO_FIELD:
-            RCLCPP_INFO(get_logger(), "Going to field");
-            do_reposition(FIELD_WAYPOINTS[4][0], FIELD_WAYPOINTS[4][1], _mission_altitude);
-            change_state_after(MissionState::RETURN, 100);
+        case MissionState::DO_TAKEOFF:
+            RCLCPP_INFO(get_logger(), "Takeoff to %.2f meters", MISSION_START_ALT);
+            do_takeoff(MISSION_START_ALT);
+
+            _takeoff_completed = false;
+            change_state_after_condition(MissionState::DO_MINE_SCAN_GOTO, [this](){
+                return _takeoff_completed;
+            });
             break;
 
-        case MissionState::RETURN:
+        case MissionState::DO_MINE_SCAN_GOTO:
+            RCLCPP_INFO(get_logger(), "Going to mine scan waypoint");
+            do_reposition(MINE_WAYPOINT[0], MINE_WAYPOINT[1], MINE_WAYPOINT[2]);
+            change_state_after_condition(
+                MissionState::DO_RTL,
+                std::bind(&MissionNode::reached_global_position, this, MINE_WAYPOINT[0], MINE_WAYPOINT[1], MINE_WAYPOINT[2]));
+            break;
+
+        case MissionState::DO_RTL:
             RCLCPP_INFO(get_logger(), "Returning to launch");
             do_return_to_launch();
             change_state(MissionState::DONE);
@@ -209,12 +183,17 @@ private:
 
         case MissionState::NOOP:
             break;
+
+        default:
+            RCLCPP_ERROR(get_logger(), "Invalid state");
+            rclcpp::shutdown();
+            break;
         }
     }
 
     void do_arm()
     {
-        px4_msgs::msg::VehicleCommand msg;
+        VehicleCommand msg;
         msg.timestamp = get_clock()->now().nanoseconds() / 1000;
         msg.target_system = 1;
         msg.target_component = 1;
@@ -228,7 +207,7 @@ private:
 
     void do_takeoff(float altitude)
     {
-        px4_msgs::msg::VehicleCommand msg;
+        VehicleCommand msg;
         msg.timestamp = get_clock()->now().nanoseconds() / 1000;
         msg.target_system = 1;
         msg.target_component = 1;
@@ -244,7 +223,7 @@ private:
 
     void do_reposition(double latitude, double longitude, float altitude)
     {
-        px4_msgs::msg::VehicleCommand msg;
+        VehicleCommand msg;
         msg.timestamp = get_clock()->now().nanoseconds() / 1000;
         msg.target_system = 1;
         msg.target_component = 1;
@@ -261,7 +240,7 @@ private:
 
     void do_return_to_launch()
     {
-        px4_msgs::msg::VehicleCommand msg;
+        VehicleCommand msg;
         msg.timestamp = get_clock()->now().nanoseconds() / 1000;
         msg.target_system = 1;
         msg.target_component = 1;
@@ -272,31 +251,62 @@ private:
         _command_pub->publish(std::move(msg));
     }
 
-    void on_mode_completed(const px4_msgs::msg::ModeCompleted &msg)
+    void on_mode_completed(const ModeCompleted::SharedPtr msg)
     {
-        RCLCPP_INFO(get_logger(), "Mode %d completed", msg.nav_state);
-        _mode_completed = true;
+        RCLCPP_INFO(get_logger(), "Mode %d completed", msg->nav_state);
+        _takeoff_completed = true;
     }
 
-    void on_global_position(const px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg)
+    void on_vehicle_status(const VehicleStatus::SharedPtr msg)
+    {
+        bool is_armed = msg->arming_state == msg->ARMING_STATE_ARMED;
+        if (is_armed != _is_armed)
+        {
+            RCLCPP_INFO(get_logger(), "Vehicle %s", is_armed ? "armed" : "disarmed");
+        }
+        _is_armed = is_armed;
+    }
+
+    void on_global_position(const VehicleGlobalPosition::SharedPtr msg)
     {
         _global_position = msg;
     }
 
-    double _mission_altitude;
+    void on_local_position(const VehicleLocalPosition::SharedPtr msg)
+    {
+        _local_position = msg;
+    }
 
-    bool _mode_completed = false;
+    bool reached_global_position(double latitude, double longitude, float altitude)
+    {
+        if (!_global_position)
+            return false;
 
-    px4_msgs::msg::VehicleGlobalPosition::SharedPtr _global_position;
+        double dx = _global_position->lat - latitude;
+        double dy = _global_position->lon - longitude;
+        double dz = _global_position->alt - altitude;
+
+        return (fabs(dx) < GLOBAL_LAT_ACCEPTANCE &&
+                fabs(dy) < GLOBAL_LON_ACCEPTANCE &&
+                fabs(dz) < GLOBAL_ALT_ACCEPTANCE);
+    }
+
+    bool _is_armed = false;
+    bool _takeoff_completed = false;
+
+    VehicleGlobalPosition::SharedPtr _global_position;
+    VehicleLocalPosition::SharedPtr _local_position;
 
     std::unique_ptr<tf2_ros::Buffer> _tf_buf;
     std::unique_ptr<tf2_ros::TransformListener> _tf_listener;
     std::unique_ptr<tf2_ros::TransformBroadcaster> _tf_broadcaster;
 
-    rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr _command_pub;
+    rclcpp::Publisher<VehicleCommand>::SharedPtr _command_pub;
 
-    rclcpp::Subscription<px4_msgs::msg::ModeCompleted>::SharedPtr _mode_completed_sub;
-    rclcpp::Subscription<px4_msgs::msg::VehicleGlobalPosition>::SharedPtr _global_position_sub;
+    rclcpp::Subscription<ModeCompleted>::SharedPtr _mode_completed_sub;
+    rclcpp::Subscription<VehicleStatus>::SharedPtr _vehicle_status_sub;
+    rclcpp::Subscription<VehicleGlobalPosition>::SharedPtr _global_position_sub;
+    rclcpp::Subscription<VehicleLocalPosition>::SharedPtr _local_position_sub;
 };
 
 int main(int argc, char *argv[])
