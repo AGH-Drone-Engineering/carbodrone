d7bc9679a1652bf78f05b26516fa028dafcb98a1
Carbon225 <carbon225@protonmail.com>
2024-08-11 22:04:17 +0200

    picamera

--- carbodrone_px/scripts/camera_reader.py
+++ carbodrone_px/scripts/camera_reader.py
@@ -1,18 +1,18 @@
-import cv2
+from picamera2 import Picamera2
 import socket
 import struct
 
 
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = Picamera2()
+    cap.configure(cap.create_preview_configuration(main={"format": 'RGB888', "size": (1280, 720)}))
+    cap.start()
 
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect('/tmp/carbodrone_px_camera')
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+        frame = cap.capture_array('main')
 
         frame_bytes = frame.tobytes()
         height = frame.shape[0]
@@ -20,9 +20,6 @@ def main():
         channels = frame.shape[2]
         sock.sendall(struct.pack('<III', height, width, channels))
         sock.sendall(frame_bytes)
-
-    cap.release()
-    sock.close()
 
 
 if __name__ == '__main__':
